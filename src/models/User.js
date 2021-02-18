4f681959f78c8e635acd1aa69ecfb1c7f734def5
unknown <alecbarba@gmail.com>
2021-02-17 20:40:53 -0600

    .

---
+++ src/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const bcrypt = require ('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+});
+
+userSchema.pre('save', function(next) { //we need this. :), that's why we need the function keyword
+    const user = this;
+
+    if(!user.isModified('password')){
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err,salt)=>{
+        if(err){
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hash)=>{
+            if(err){
+                return next(err);
+            }
+
+            user.password = hash;
+            next();
+        })
+    })
+
+});
+
+
+userSchema.methods.comparePassword= function(candidatePassword) {
+    const user = this;
+    return new Promise((resolve, reject)=>{
+        bcrypt.compare(candidatePassword, user.password, (err, isMatch)=>{
+            if( err){
+                return reject (err);
+            }
+
+            if (!isMatch){
+                return reject(false);
+            }
+
+            resolve(true);
+        });
+    })
+}
+
+mongoose.model('User', userSchema);
